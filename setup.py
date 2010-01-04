11b9fb93815cd5d200448251261ba077a5179800
ivank <ivank@ludios.net>
2010-01-04 15:42:42 -0800

    add setup.py and docs/monotonic-timers.html

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='Monoclock',
+	version='10.1.4',
+	description="Monotonic clock access for Python",
+	packages=['monoclock'],
+)
