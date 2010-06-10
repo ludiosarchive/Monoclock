8c5ab094a0ee74ac47d852dea2b0c664fdb81bb4
ivank <ivank@ludios.net>
2010-06-10 06:54:58 +0000

    test_monoclock.py: Use `assertTrue` instead of `assert_`

--- test_monoclock.py
+++ test_monoclock.py
@@ -17,7 +17,7 @@ class MonoclockTests(unittest.TestCase):
 	def test_nano_count_isCorrectType(self):
 		val1 = monoclock.nano_count()
 		# In reality it is really always a long, I think.
-		self.assert_(isinstance(val1, (int, long)))
+		self.assertTrue(isinstance(val1, (int, long)))
 
 
 	def test_nano_count_getsBigger(self):
